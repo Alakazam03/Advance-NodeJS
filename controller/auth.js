717509a5e2db576f435b1befee458823dbee1058
Alakazam03 <vaibhav.045@iiitb.org>
2020-03-30 14:29:53 +0530

    encrypt pwd

--- controller/auth.js
+++ controller/auth.js
@@ -6,7 +6,18 @@ const User = require('../models/User');
 // @desc Register user
 // @route /api/v1/auth/register
 exports.register = asyncHandler(async(req, res, next) => {
+    const { name, email, password, role } = req.body;
+
+    const user = await User.create({
+        name,
+        email, 
+        password,
+        role
+    })
+
     res.status(200).json({
-        'success': true
+        'success': true,
+        message: 'user added.',
+        data: user
     })
 })
