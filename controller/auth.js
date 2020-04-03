17c6542ab20a92e82f8e34943fd382988b3fe914
Alakazam03 <vaibhav.045@iiitb.org>
2020-04-03 14:18:04 +0530

    Update user details only name, email

--- controller/auth.js
+++ controller/auth.js
@@ -130,6 +130,25 @@ exports.resetPassword = asyncHandler( async(req, res, next) => {
 	sendTokenResponse(user, 200, res);
 })
 
+// @desc Update user details
+// @route PUT /api/v1/auth/updatedetails
+// @access Private
+exports.updateDetails = asyncHandler(async(req, res, next) => {
+    const fieldToUpdate = {
+        name: req.body.name,
+        email: req.body.email
+    }
+    const user = await User.findByIdAndUpdate(req.user.id, fieldToUpdate, {
+        new: true,
+        runValidators: true
+    });
+	
+	res.status(200).json({
+		status: 'success',
+		data: user
+	})
+})
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
     const token = user.getSignedJwtToken();
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {register, login, getMe, forgotPassword, resetPassword} = require('../controller/auth');
+const {register, login, getMe, forgotPassword, resetPassword, updateDetails} = require('../controller/auth');
 const { protect } = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe)
+router.put('/updatedetails', protect, updateDetails)
 router.post('/forgotpassword', forgotPassword)
 router.put('/resetpassword/:resetToken', resetPassword)
 
