a04b0c7a55509cfb794a6e7663a56350487f5c75
Alakazam03 <vaibhav.045@iiitb.org>
2020-04-04 23:26:00 +0530

    average ratings for bootcamps

--- models/Review.js
+++ models/Review.js
@@ -36,4 +36,39 @@ const ReviewSchema = new mongoose.Schema({
 // Prevent user from adding more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1}, { unique: true});
 
+// Statics calculate 
+// get out all ratings for a specific bootcamp
+// calculate average and assign that to the bootcamp
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+    console.log("Calculating average ratings");
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageRating: { $avg: '$rating'}
+            }
+        }
+    ]);
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageRating
+        })
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Call getAverageRating after save
+ReviewSchema.post('save', function() {
+    this.constructor.getAverageRating(this.bootcamp)
+})
+
+// Call getAverageRating before remove
+ReviewSchema.pre('remove', function() {
+    this.constructor.getAverageRating(this.bootcamp)
+})
+
 module.exports = mongoose.model('Review', ReviewSchema)
