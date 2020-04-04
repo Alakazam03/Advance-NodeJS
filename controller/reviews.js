adcf27754c7e3c22f64fa7584a1bdc2f1a1adaef
Alakazam03 <vaibhav.045@iiitb.org>
2020-04-04 23:05:21 +0530

    added reviews to bootcamp

--- controller/reviews.js
+++ controller/reviews.js
@@ -42,3 +42,26 @@ exports.getReview = asyncHandler(async (req, res, next) => {
         data: review
     })
 });
+
+// @desc Add single review
+// @route POST /api/v1/bootcamps/:bootcampId/reviews
+// @access Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+    req.body.bootcamp = req.params.bootcampId;
+    req.body.user = req.user.id;
+
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if (!bootcamp) {
+        return next(
+            new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+        );
+    }
+
+    const review = await Review.create(req.body);
+
+    res.status(201).json({
+        status: 'success',
+        data: review
+    })
+});
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getReviews, getReview} = require('../controller/reviews')
+const { getReviews, getReview, addReview} = require('../controller/reviews')
 
 const advancedResults = require('../middleware/advancedResults');
 const Review = require('../models/Review');
@@ -13,7 +13,8 @@ router
     .get(advancedResults(Review, {
             path: 'Bootcamp',
             select: 'name decription'
-        }), getReviews);
+        }), getReviews)
+    .post(protect, authorize('user', 'admin'), addReview)
 
 router
     .route('/:id')
